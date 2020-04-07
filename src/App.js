9965740927e71b670e6f0e61dad993fccaa15616
benjamin lin <blin432@gmail.com>
2020-04-07 15:08:16 -0400

    added lyrics cards and track cards

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Navbar from './components/layout/Navbar.js';
 import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
 import Index from './components/layout/Index.js';
 import {Provider} from './context.js';
+import Lyrics from './components/tracks/Lyrics.js';
 
 function App() {
   return (
@@ -14,6 +15,7 @@ function App() {
           <div className= "container">
             <Switch>
               <Route exact path='/' component={Index}/>
+              <Route exact path="/lyrics/track/:id" component={Lyrics}/>
             </Switch>
           </div>
         </React.Fragment>
---
+++ src/components/layout/Spinner.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+
+class Spinner extends Component {
+    render() {
+        return (
+            <div>
+                <img src={"https://ak8.picdn.net/shutterstock/videos/1009250978/thumb/1.jpg"} alt="loading" style={{width:'30px auto', display:'block'}}/>
+            </div>
+        )
+    }
+}
+
+export default Spinner;
